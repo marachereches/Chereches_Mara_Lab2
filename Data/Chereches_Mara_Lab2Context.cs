54f408f903b7b12dba6cb8fcc803dd3fd3438619
marachereches <marachereches@yahoo.com>
2022-10-30 18:58:21 +0200

    Add project files.

---
+++ Data/Chereches_Mara_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Chereches_Mara_Lab2.Models;
+
+namespace Chereches_Mara_Lab2.Data
+{
+    public class Chereches_Mara_Lab2Context : DbContext
+    {
+        public Chereches_Mara_Lab2Context (DbContextOptions<Chereches_Mara_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Chereches_Mara_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Chereches_Mara_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Chereches_Mara_Lab2.Models.Author> Author { get; set; }
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Chereches_Mara_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
